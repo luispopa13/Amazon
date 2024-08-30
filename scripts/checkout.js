f62b39591e32f03a29a49c830a8e5e30a3dfbea4
PopaAndreasLuis <andreasluispopa@gmail.com>
2024-08-30 17:46:48 +0300

    Regenerate HTML when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,8 @@ import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../data/deliveryOptions.js';
 
+function renderOrderSummary(){
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -212,7 +214,9 @@ document.querySelectorAll('.js-delivery-option')
             const productId = element.dataset.productId;
             const deliveryOptionId = element.dataset.deliveryOptionId;
             updateDeliveryOption(productId, deliveryOptionId);
+            renderOrderSummary();
         });
     });
+};
 
-
+renderOrderSummary();
