54c1dbdb4e2f90d97c9dba959408a19ca0e00a8f
PopaAndreasLuis <andreasluispopa@gmail.com>
2024-09-04 22:55:46 +0300

    Convert loadProducts from XHR to fetch

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,8 @@
 import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
-import {products, loadProducts} from '../data/products.js';
+import {products, loadProductsFetch} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-loadProducts(renderProductsGrid);
+loadProductsFetch(renderProductsGrid);
 
 function renderProductsGrid() {
 
